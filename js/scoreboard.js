4a5551d3016e9b7b1a357232edd5ecab155875e2
sudhirjo <sudhirsalunke199@gmail.com>
2024-10-23 17:32:53 +0530

    code

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -11,41 +11,115 @@ const moreRuns = document.querySelector(".js-more-runs")
 const moreRunsDropDown = document.querySelector(".more-runs__drop-down")
 const runs = document.querySelectorAll(".runs")
 const over = document.querySelector(".over-runs")
+let runSide = document.querySelector(".run-side")
+
+// Batsman side
+
+let batsman1 = document.getElementById("batsman-1")
+let batsman2 = document.getElementById("batsman-2")
+
+batsman1.textContent = "-"
+batsman2.textContent = "-"
+
+let batsman1Runs = document.getElementById("batsman-1-runs")
+let batsman2Runs = document.getElementById("batsman-2-runs")
+
+batsman1Runs.textContent = "0"
+batsman2Runs.textContent = "0"
+
+let batsman1Balls = document.getElementById("batsman-1-balls")
+let batsman2Balls = document.getElementById("batsman-2-balls")
+
+batsman1Balls.textContent = 0
+batsman2Balls.textContent = 0
+
+let batsman1Fours = document.getElementById("batsman-1-fours")
+let batsman2Fours = document.getElementById("batsman-2-fours")
+
+batsman1Fours.textContent = "0"
+batsman2Fours.textContent = "0"
+
+let batsman1Six = document.getElementById("batsman-1-six")
+let batsman2Six = document.getElementById("batsman-2-six")
+
+batsman1Six.textContent = "0"
+batsman2Six.textContent = "0"
+
+let batsman1SR = document.getElementById("batsman-1-sr")
+let batsman2SR = document.getElementById("batsman-2-sr")
+
+batsman1SR.textContent = "0"
+batsman2SR.textContent = "0"
+
+// Bowler side
+
+let bowler1 = document.getElementById("bowler-name")
+
+bowler1.textContent = "-"
+
+let bowlerRuns = document.getElementById("bowler-runs")
+bowlerRuns.textContent = "0"
+
+let bowlerBalls = document.getElementById("bowler-balls")
+bowlerBalls.textContent = "0"
+
+let bowlerFours = document.getElementById("bowler-fours")
+bowlerFours.textContent = "0"
+
+let bowlerSix = document.getElementById("bowler-six")
+bowlerSix.textContent = "0"
+
+let bowlerSR = document.getElementById("bowler-sr")
+bowlerSR.textContent = "0"
+
+
+let extras = document.getElementById("extra")
+extras.textContent = "0"
+
+let partnershipRuns = document.getElementById("partnership-runs")
+partnershipRuns.textContent = "0"
+
+let partnershipBalls = document.getElementById("partnership-balls")
+partnershipBalls.textContent = "0"
+
+let matchRunRate = document.getElementById("match-run-rate")
+matchRunRate.textContent = "0"
+
+let overBalls = document.getElementById("over-balls")
+overBalls.textContent = "0"
+
+let totalOvers = document.getElementById("total-overs")
+totalOvers.textContent = "0"
+
 let overEnd = 0;
 
 let runsOfTheOver = []
-
-
 
 
 let openedTab;
 
 scoreTabs.forEach((tabs) => {
     tabs.addEventListener("click", () => {
-        scoreTabs.forEach((tabs) => tabs.setAttribute("aria-label","false"))
-        tabs.setAttribute("aria-label","true")
+        scoreTabs.forEach((tabs) => tabs.setAttribute("aria-label", "false"))
+        tabs.setAttribute("aria-label", "true")
     })
 })
 
 
 runBtn.forEach((btn) => {
 
-    btn.addEventListener("click", () => {    
-            openedTab = btn
-            LBOptions.forEach((opt) => {
+    btn.addEventListener("click", () => {
+        openedTab = btn
+        LBOptions.forEach((opt) => {
 
-                if (openedTab.textContent.trim() === opt.dataset.option) {
-                    opt.style.display = "flex"
-                    runOptions.style.display = "none"
-                }
-                else {
-                    opt.style.display = "none"
-                }
-            })
-        
-
-
-
+            if (openedTab.textContent.trim() === opt.dataset.option) {
+                opt.style.display = "flex"
+                runOptions.style.display = "none"
+            }
+            else {
+                opt.style.display = "none"
+            }
+        })
     })
 })
 
@@ -53,22 +127,36 @@ runs.forEach((run) => {
     run.addEventListener("click", () => {
         runsOfTheOver.push(run.textContent.trim());
 
-        if(overEnd !== 6)
-        {
-
+        if (overEnd !== 6) {
             overEnd += run.textContent.trim().includes("NB") || run.textContent.trim().includes("WB") ? 0 : 1
-            
-           over.innerHTML = runsOfTheOver.map((roy) => {
-            return `<div class="over-runs__circle-wrap --${roy}">
+            over.innerHTML = runsOfTheOver.map((roy) => {
+                return `<div class="over-runs__circle-wrap --${roy}">
                        ${roy}
                     </div>`;
-            }).join('');        
-     
+            }).join('');
+
+            over.scrollLeft = over.scrollWidth;
+            let initialValue = 0;
+            let matchScore = runsOfTheOver.reduce((accumulator, currentValue) => {
+                if (currentValue.includes("WB")) {
+                    let wideRuns = parseInt(currentValue) || 0;
+                    return accumulator + wideRuns + 1; 
+                } else if (currentValue.includes("NB")) {
+                    let noBallRuns = parseInt(currentValue) || 0;
+                    return accumulator + noBallRuns + 1; 
+                } else {
+                    return accumulator + parseInt(currentValue); 
+                }
+            }, initialValue);
+
+            runSide.textContent = matchScore
+
         }
-        else{
+        else {
             runsOfTheOver = []
             over.innerHTML = ''
-        } 
+            overEnd = 0;
+        }
     });
 });
 
@@ -78,7 +166,7 @@ plusMinus.forEach((icon) => {
         e.stopPropagation();
         plusMinus.forEach((i) => i.ariaSelected = false)
         icon.setAttribute("aria-selected", "true")
-        
+
         togglePlusMinus.forEach((tpm) => {
             tpm.textContent = icon.textContent
         })
@@ -122,14 +210,14 @@ closeLBOptions.forEach((cls) => {
 
 })
 
-    moreRuns.addEventListener("click" , (event) => {
-        event.stopPropagation();
-        moreRunsDropDown.setAttribute("aria-hidden", "false");
-        
-    })
+moreRuns.addEventListener("click", (event) => {
+    event.stopPropagation();
+    moreRunsDropDown.setAttribute("aria-hidden", "false");
 
-    document.addEventListener("click", () => {
-        if (moreRunsDropDown.getAttribute("aria-hidden") === "false") {
-            moreRunsDropDown.setAttribute("aria-hidden", "true");
-        }
-    })
+})
+
+document.addEventListener("click", () => {
+    if (moreRunsDropDown.getAttribute("aria-hidden") === "false") {
+        moreRunsDropDown.setAttribute("aria-hidden", "true");
+    }
+})
